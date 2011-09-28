ecd535b0f8b32681aca33b2f67e946a3b249ff65
Imanol Eizaguirre <imanol@imanol.cc>
2011-09-28 13:49:34 +0200

    Cleaner code

--- simplecrypto.py
+++ simplecrypto.py
@@ -11,30 +11,29 @@ def aes_build_cipher(key, iv, op=ENC):
     return M2Crypto.EVP.Cipher(alg='aes_128_ecb', key=key, iv=iv, op=op)
 
 
-def aes_encryptor(key, msg, iv=None):
-    """"""
-    def encrypt(data):
-        iv = '\0' * 16
-        cipher = aes_build_cipher(key.decode("hex"), iv, ENC)
-        v = cipher.update(data)
-        v = v + cipher.final()
-        del cipher
-        v = v.encode("hex")
+def aes_encryptor(key, data):
+    """
+    AES 128 ECB Encryptor
+    """
+    iv = '\0' * 16
+    cipher = aes_build_cipher(key.decode("hex"), iv, ENC)
+    v = cipher.update(data)
+    v = v + cipher.final()
+    del cipher
+    v = v.encode("hex")
 
-        return v
-
-    return encrypt(msg)
+    return v
 
 
-def aes_decryptor(key, msg, iv=None):
-    """"""
-    def decrypt(data):
-        iv = '\0' * 16
-        data = data.decode("hex")
-        cipher = aes_build_cipher(key.decode("hex"), iv, DEC)
-        v = cipher.update(data)
-        v = v + cipher.final()
-        del cipher
-        return v
+def aes_decryptor(key, data):
+    """
+    AES 128 ECB Decryptor
+    """
+    iv = '\0' * 16
+    data = data.decode("hex")
+    cipher = aes_build_cipher(key.decode("hex"), iv, DEC)
+    v = cipher.update(data)
+    v = v + cipher.final()
+    del cipher
 
-    return decrypt(msg)
+    return v
